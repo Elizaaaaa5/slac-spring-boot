f1bab069fd8436376906a7f4d7c9fc88cc4a660f
Elizaaaaa5 <luyao9974@gmail.com>
2019-10-08 14:19:00 -0400

    Done

--- lab-01-basic/src/main/java/com/sample/SampleApplication.java
+++ lab-01-basic/src/main/java/com/sample/SampleApplication.java
@@ -1,12 +1,19 @@
 package com.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class SampleApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(SampleApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Hello, I am an info logger.");
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
