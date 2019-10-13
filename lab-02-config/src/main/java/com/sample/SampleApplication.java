580d7981e2e66ec44cf5f614adaa8a84501a7b90
Elizaaaaa5 <luyao9974@gmail.com>
2019-10-13 14:04:39 -0400

    Completed.

--- lab-02-config/src/main/java/com/sample/SampleApplication.java
+++ lab-02-config/src/main/java/com/sample/SampleApplication.java
@@ -1,13 +1,28 @@
 package com.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SampleApplication {
 
+	@Value("${application.properties}")
+	private static String propertyKey = "property-key";
+
+	private static final Logger logger = LoggerFactory.getLogger(SampleApplication.class);
+
 	public static void main(String[] args) {
+		logMe();
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
+	@PostConstruct
+	public static void logMe(){
+		logger.info(propertyKey);
+	}
 }
